5cca358e1af128ebd5913ad83005b1558e0ddcc0
LI <lrhdiy@gmail.com>
2016-07-11 17:08:38 +0800

    update

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -1,15 +1,20 @@
-cd ~/.vim_runtime
+ cd ~/.vim_runtime
 
-echo 'set runtimepath+=~/.vim_runtime
+ echo 'set runtimepath+=~/.vim_runtime
 
-source ~/.vim_runtime/vimrcs/basic.vim
-source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
+ source ~/.vim_runtime/vimrcs/basic.vim
+ source ~/.vim_runtime/vimrcs/filetypes.vim
+ source ~/.vim_runtime/vimrcs/plugins_config.vim
+ source ~/.vim_runtime/vimrcs/extended.vim
 
-try
-source ~/.vim_runtime/my_configs.vim
-catch
-endtry' > ~/.vimrc
+ try
+ source ~/.vim_runtime/my_configs.vim
+ catch
+ endtry
 
-echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+ set background=dark
+ colorscheme solarized
+ set shiftwidth=2
+ ' > ~/.vimrc
+
+ echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
